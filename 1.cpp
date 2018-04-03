944ad5f698a0e7066d4ab9c6fe4cf9b13b73b147
Troy <Troy@exapmple.com>
2018-04-03 22:34:55 +0530

    v1.00

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a,b,c;
+int sum(int ,int);
+
+int mul(int ,int);
+
+int sub(int ,int);
+int main()
+{
+
+cout<<sum(a,b)
+;
+cout<<mul(a,b);
+cout<<sub(a,b);
+
+return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a;
+int noOfPrimes(int);
+
+int main()
+{
+
+cout<<noOfPrimes(a);
+
+return 0;
+}
